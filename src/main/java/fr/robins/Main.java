7d0d7814bc9db984561284f6fdddc8e6ee5fd312
robis <robin.simonneau@etudiant.univ-reims.fr>
2024-04-30 16:09:58 +0200

    Ajout de la boucle de gameplay

--- src/main/java/fr/robins/Main.java
+++ src/main/java/fr/robins/Main.java
@@ -1,7 +1,34 @@
 package fr.robins;
 
-public class Main {
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 600,400);
+
+        stage.setScene(scene);
+        stage.setTitle("Not the legend of zelda");
+        stage.show();
+
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                //Boucle de gameplay
+
+            }
+        };
+        gameLoop.start();
     }
 }
