836b3b673871d79c0e0f041b03240488a8292306
RobinS <robin.le.dev@gmail.com>
2024-05-06 11:37:06 +0200

    Ajout d'un exception si il y a un problème lors du chargement de la scène de combat

---
+++ src/main/java/fr/robins/types/exceptions/CombatLoadingException.java
@@ -0,0 +1,7 @@
+package fr.robins.types.exceptions;
+
+public class CombatLoadingException extends Exception{
+    public CombatLoadingException(String errorMessage){
+        super(errorMessage);
+    }
+}
