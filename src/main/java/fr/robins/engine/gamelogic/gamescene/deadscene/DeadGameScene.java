6c336a16bfda4e14599cfba344ff67d96de8962a
RobinS <robin.le.dev@gmail.com>
2024-05-06 19:50:42 +0200

    Ajout d'un écran de mort

---
+++ src/main/java/fr/robins/engine/gamelogic/gamescene/deadscene/DeadGameScene.java
@@ -0,0 +1,24 @@
+package fr.robins.engine.gamelogic.gamescene.deadscene;
+
+import fr.robins.engine.gamelogic.gamescene.GameScene;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DeadGameScene extends GameScene {
+    public DeadGameScene() {
+        super();
+
+
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            InputStream fxmlStream = getClass().getResourceAsStream("/sceneBuilder/combatScene.fxml");
+
+            pane = loader.load(fxmlStream);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error have occured while loading the scene :  " + e);
+        }
+    }
+}
